9ab9125bd0508c5dc8b969aa5be025208dde2216
Your Name <you@example.com>
2023-07-29 00:26:37 +0530

    basic layout

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+let cl = console.log()
+
+const navigation = document.getElementById("navigation")
+
+
+const navToggle = document.getElementById("navToggle")
+
+
+
+
+
+
+navToggle.addEventListener("click",()=>{
+    navigation.classList.toggle("active")
+} )
